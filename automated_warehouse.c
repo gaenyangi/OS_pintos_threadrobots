c1802c80647ca0a360ce5ab33be92f354f7976a7
Hongjunyeong <105625862+gaenyangi@users.noreply.github.com>
2024-04-30 02:53:03 -0700

    Add files via upload

--- automated_warehouse.c
+++ automated_warehouse.c
@@ -14,64 +14,66 @@ int robot_count = 0; // total number of robots
 int robot_current = 1; // current moving robot. just used for cnt thread, not to intercross robots.
 
 struct robot* robots;
-/* test code for central control node thread
-void test_cnt(){
-        while(1){
-                print_map(robots, 4);
-                thread_sleep(1000);
-                block_thread();
-        }
-}
-
-test code for robot thread
-void test_thread(void* aux){
-        int idx = *((int *)aux);
-        int test = 0;
-        while(1){
-                printf("thread %d : %d\n", idx, test++);
-                thread_sleep(idx * 1000);
-        }
-}*/
 
 void cnt() {
-    while (robot_current<robot_count) {
+    while (robot_current<robot_count+1) {
 
         print_map(robots, robot_count);
-        //thread_sleep(1000);
-        block_thread();
-        recieve_message_from_robot(robot_current);
-        if (i == robot_current) {
-            if (recieve_message_from_robot(i).current_payload == recieve_message_from_robot(i).required_payload
-                && recieve_message_from_robot(i).row == recieve_message_from_robot(i).tar_row
-                && recieve_message_from_robot(i).col == recieve_message_from_robot(i).tar_col) {
-                robot_current++;// if robot gets to destination with req, move the next robot.
-                break;
+        for (int i = 1; i < robot_count+1; i++) {
+            recieve_message_from_robot(i);
+
+            if (i == robot_current) {
+                if (recieve_message_from_robot(i).current_payload == recieve_message_from_robot(i).required_payload
+                    && recieve_message_from_robot(i).row == recieve_message_from_robot(i).tar_row
+                    && recieve_message_from_robot(i).col == recieve_message_from_robot(i).tar_col) {
+                    robot_current++;// if robot gets to destination with req, move the next robot.
+                    break;
+                }
+                if else (recieve_message_from_robot(i).current_payload == recieve_message_from_robot(i).required_payload) { //check payload status
+                    recieve_message_from_robot(i).cmd = 1; //if payload success, move to target destination
+                    send_message_to_robot(i, recieve_message_from_robot(i));
+                }
+                else {
+                    recieve_message_from_robot(i).cmd = 2; // haven't get payload, move to req
+                    send_message_to_robot(i, recieve_message_from_robot(i));
+                }
             }
-            if (recieve_message_from_robot(i).current_payload == recieve_message_from_robot(i).required_payload) { //check payload status
-                recieve_message_from_robot(i).cmd = 1; //if payload success, move to target destination
-                send_message_to_robot(i, recieve_message_from_robot(i));
-            }
-            else {
-                recieve_message_from_robot(i).cmd = 2; // haven't get payload, move to req
+            else { //set other robots as waiting
+                recieve_message_from_robot(i).cmd = 0;
                 send_message_to_robot(i, recieve_message_from_robot(i));
             }
         }
-        else { //set other robots as waiting
-            recieve_message_from_robot(i).cmd = 0;
-            send_message_to_robot(i, recieve_message_from_robot(i));
-        }
-         
+        unblock_threads();
         increase_step();
     }
 }
 
 void robot_thread(void* aux) { // robot_thread implement
-    int idx = *((int*)aux);
-    int test = 0;
+    int idx = *((int*)aux);   
+    printf("thread for robot %d \n",idx);
     while (1) {
-        //printf("thread %d : %d\n", idx, test++);
-        //thread_sleep(idx * 1000);
+        int cmd=recieve_message_from_cnt(idx);
+        switch(cmd){
+        case 0:
+            //wait
+            break;
+        case 1:
+            move_robot_to_tar((robots+idx));
+            break;
+        case 2:
+            move_robot_to_req((robots+idx));
+            break;
+        }
 
+        boxes_from_robots[idx]->msg.row = robots[idx].row;
+        boxes_from_robots[idx]->msg.col = robots[idx].col;
+        boxes_from_robots[idx]->msg.current_payload = robots[idx].current_payload;
+        boxes_from_robots[idx]->msg.required_payload = robots[idx].required_payload;
+        boxes_from_robots[idx]->msg.tar_row = robots[idx].tar_row;
+        boxes_from_robots[idx]->msg.tar_col = robots[idx].tar_col;
+
+        send_message_to_cnt(robots[idx], boxes_from_robots[idx]->msg);
+        block_thread();
     }
 }
 
@@ -81,17 +83,7 @@ void run_automated_warehouse(char **argv)
 {
         init_automated_warehouse(argv); // do not remove this
 
-        printf("implement automated warehouse!\n");
-
-        /*
-        test case robots
-
-        setRobot(&robots[0], "R1", 5, 5, 0, 0);
-        setRobot(&robots[1], "R2", 0, 2, 0, 0);
-        setRobot(&robots[2], "R3", 1, 1, 1, 1);
-        setRobot(&robots[3], "R4", 5, 5, 0, 0); */ 
-
-        
+        printf("implement automated warehouse!\n");       
 
         robot_count = atoi(argv[1]);
         robots = malloc(sizeof(struct robot) * robot_count);
@@ -114,7 +106,7 @@ void run_automated_warehouse(char **argv)
             */
 
             if (relase_pos == 'A') { target_row = 0; target_col = 2; }
-            else if (relase_pos == 'A') { target_row = 2; target_col = 0; }
+            else if (relase_pos == 'B') { target_row = 2; target_col = 0; }
             else { target_row = 5; target_col = 2; }
 
             char robot_name[5];
@@ -122,25 +114,13 @@ void run_automated_warehouse(char **argv)
             setRobot(&robots[i], robot_name, 5, 5, target_load, 0,target_row, target_col);
         }
 
-
-
-
-        /* example of create thread
-        tid_t* threads = malloc(sizeof(tid_t) * robot_count+1);
-        int idxs[4] = {1, 2, 3, 4};
-        threads[0] = thread_create("CNT", 0, &test_cnt, NULL);
-        threads[1] = thread_create("R1", 0, &test_thread, &idxs[1]);
-        threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
-        threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
-        */
-
         tid_t* threads = malloc(sizeof(tid_t) * (robot_count + 1));
         int* idxs = malloc(sizeof(int) * (robot_count + 1));
         threads[0] = thread_create("CNT", 0, &cnt, NULL);
         for (int i = 1; i < robot_count + 1; i++) {
             char thread_name[5];
             sprintf(thread_name, "R&d", i);
-            threads[i] = thread_create(thread_name, 0, &robot_thread, *idxs[i]));
+            threads[i] = thread_create(thread_name, 0, &robot_thread, &idxs[i]);
         }
 
 
